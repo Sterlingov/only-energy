2e020f87066e2a7690261afb03b4fc6beacdfa95
Sterlingov <89503351+Sterlingov@users.noreply.github.com>
2024-03-19 00:53:25 +0300

    Какие то изменения agashi

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 var dogeImage = document.getElementById("dogeImage"); // берем нужный элемент по айдишнику
+var text = document.getElementById("papaotdaymnedevstvennost");
 
 function showDoge() { 
     var request = new XMLHttpRequest(); // создаем новый запрос
-    request.open("GET", "https://random.dog/woof.json"); // открываем запрос, первый аргумент - метод GET, второй - адрес ресурса
+    request.open("GET", "https://yesno.wtf/api"); // открываем запрос, первый аргумент - метод GET, второй - адрес ресурса
     request.responseType = "json"; // даем понять что ждем в ответ json объект
     request.onload = function(){ // показываем че делать, когда придет ответ
-        dogeImage.src = request.response.url; // устанавливаем атрибут src. Передаем туда ссылку на картинку. Ссылку мы получили от сервера
+        dogeImage.src = request.response.image; // устанавливаем атрибут src. Передаем туда ссылку на картинку. Ссылку мы получили от сервера
+        text.innerHTML = request.response.answer;
     }
     request.send(); // отправляем запрос, после получения ответа на него выполнится строчка 8
 }
