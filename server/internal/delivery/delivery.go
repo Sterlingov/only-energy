1aa26530a49da2fca8318452183e0a603bab0466
Sterlingov <forwardeleven13@gmail.com>
2024-03-27 20:46:19 +0300

    modified:   server/internal/delivery/delivery.go

--- server/internal/delivery/delivery.go
+++ server/internal/delivery/delivery.go
@@ -9,8 +9,8 @@ import (
 
 func GetPostsHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	w.Write(service.GetPosts())
 	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Write(service.GetPosts())
 }
 
 func GetImageHandler(w http.ResponseWriter, r *http.Request) {
